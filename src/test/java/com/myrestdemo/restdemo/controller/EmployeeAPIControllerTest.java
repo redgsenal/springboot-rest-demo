a7c008a8a86b061d7f14602c54d0d1c83f0fe868
Reggie <reggie.senal@gmail.com>
2024-04-25 21:55:13 +0800

    add controller test

---
+++ src/test/java/com/myrestdemo/restdemo/controller/EmployeeAPIControllerTest.java
@@ -0,0 +1,41 @@
+package com.myrestdemo.restdemo.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(EmployeeAPIController.class)
+class EmployeeAPIControllerTest {
+
+    
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getEmployeeList() {
+    }
+
+    @Test
+    void createEmployee() {
+    }
+
+    @Test
+    void getEmployeeDetails() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+}
